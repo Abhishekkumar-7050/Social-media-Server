51ad60bcb7e42d787334dd4953b3467110315de2
Abhishekkumar-7050 <abhishekofficial7050>
2024-05-03 17:41:38 +0530

    set cores

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(morgon('common'));
 app.use(cookieParser());
 app.use(cors({
     credentials:true,
-    origin:'http://localhost:3000',
+    origin:'https://social-media-client-vert.vercel.app/'
 }))
 
 
